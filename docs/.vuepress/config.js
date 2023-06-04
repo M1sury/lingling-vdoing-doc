ef9f03ab8b6b23beb63c01aa2255b1100156d531
misury <xx1021@163.com>
2023-06-04 16:17:57 +0800

    Revert "fix: 修改一键复制插件"
    This reverts commit 6d2d5b7f

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -295,10 +295,9 @@ module.exports = {
                 hm: baiduCode || '30f324f9ae581aecfa25b75a6a33ab4d'
             }
         ],
-        ['vuepress-plugin-one-click-copy', { // 代码块复制按钮
+        ['one-click-copy', { // 代码块复制按钮
             copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-            copyMessage: 'Copied successfully!', // default is 'Copy successfully and then paste it for use.'
-            toolTipMessage: 'Copy to clipboard',
+            copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
             duration: 1000, // prompt message display time.
             showInMobile: false // whether to display on the mobile side, default: false.
         }],
