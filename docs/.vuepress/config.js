6d2d5b7f0df6d5df9fd494c79f46cff828cee1e3
misury <xx1021@163.com>
2023-06-04 10:44:09 +0800

    fix: 修改一键复制插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -295,9 +295,10 @@ module.exports = {
                 hm: baiduCode || '30f324f9ae581aecfa25b75a6a33ab4d'
             }
         ],
-        ['one-click-copy', { // 代码块复制按钮
+        ['vuepress-plugin-one-click-copy', { // 代码块复制按钮
             copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-            copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+            copyMessage: 'Copied successfully!', // default is 'Copy successfully and then paste it for use.'
+            toolTipMessage: 'Copy to clipboard',
             duration: 1000, // prompt message display time.
             showInMobile: false // whether to display on the mobile side, default: false.
         }],
