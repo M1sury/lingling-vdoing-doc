87dbfba6a539cf5148266e547b2b1f19e06f0684
misury <xx1021@163.com>
2023-06-12 21:04:08 +0800

    feat: 增加第12小节

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -44,6 +44,7 @@ module.exports = {
                         text: '第二章：对于所有对象都通用的方法', items: [
                             {text: '10.覆盖equals时请遵守通用约定', link: '/pages/8d58b1/'},
                             {text: '11.覆盖equals时总要覆盖hashCode', link: '/pages/e736d2/'},
+                            {text: '12.始终要覆盖toString', link: '/pages/7814b8/'},
                         ]
                     },
                     // { text: '第三章：类和接口', items:[
